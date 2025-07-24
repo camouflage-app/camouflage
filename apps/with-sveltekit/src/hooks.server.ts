00cdd1a04537835781f14fe65f292d033fb58700
beltebelt <shubhendumadhukar@mailo.com>
2025-07-24 14:47:37 +0530

    remove svelte/sveltekit example

--- apps/with-sveltekit/src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import type { ServerInit } from "@sveltejs/kit";
-import CamouflageHttp from "@camouflage/http";
-export const init: ServerInit = () => {
-    const server = new CamouflageHttp();
-    server.loadConfigFromJson("/Users/shubhendumadhukar/Documents/camouflage/apps/with-sveltekit/config.json");
-    server.start();
-}
